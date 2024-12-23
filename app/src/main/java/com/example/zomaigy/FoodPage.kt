936c849378a8e9d7866eb95fb2f336ee0bc9acdc
namanhere23 <namanhere23code@gmail.com>
2024-12-23 17:12:31 +0530

    ZOMAIGY

---
+++ app/src/main/java/com/example/zomaigy/FoodPage.kt
@@ -0,0 +1,32 @@
+package com.example.zomaigy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class FoodPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_food_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var btn=findViewById<MaterialButton>(R.id.prevorder)
+
+        btn.setOnClickListener{
+            Toast.makeText(this, "Previous Orders", Toast.LENGTH_SHORT).show()
+            println("ButtonClicked")
+            var intent= Intent(this, YourOrders::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zomaigy/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.zomaigy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var btn=findViewById<MaterialButton>(R.id.cont)
+
+        btn.setOnClickListener{
+            Toast.makeText(this, "Welcome To ZOMAIGY", Toast.LENGTH_SHORT).show()
+            println("ButtonClicked")
+            var intent= Intent(this, FoodPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
